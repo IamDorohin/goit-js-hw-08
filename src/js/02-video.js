0ae066dccde8eb2819df21ad5951328180414eca
Kostiantyn Dorohin <iamdorohin@gmail.com>
2022-09-11 13:02:27 +0300

    Виконує друге завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import Throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+const currentTime = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+setCurrentTime();
+
+player.on('timeupdate', Throttle(onCurrentTime, 1000));
+
+function onCurrentTime({seconds}) {
+localStorage.setItem(STORAGE_KEY, seconds);
+};
+
+function setCurrentTime() {
+    player.setCurrentTime(currentTime);
+};
