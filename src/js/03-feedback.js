f17eae04631462aaeba47a5578287590a2847648
Kostiantyn Dorohin <iamdorohin@gmail.com>
2022-09-11 14:33:11 +0300

    Виконує домашнє завдання повністю

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import Throttle from 'lodash.throttle';
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.input.addEventListener('input', Throttle(onInputChange, 500)); 
+refs.textarea.addEventListener('input', Throttle(onTextareaChange, 500));
+
+const STORAGE_KEY = "feedback-form-state";
+const storageValues = {};
+
+onDataReturn();
+
+function onInputChange(evt) {
+    inputEmail = evt.target.value;
+
+    storageValues.email = inputEmail;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(storageValues));
+};
+
+function onTextareaChange(evt) {
+    textareaMessage = evt.target.value;
+
+    storageValues.textarea = textareaMessage;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(storageValues));
+};
+
+function onDataReturn() {
+    const parsedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if(parsedData) {
+        refs.input.value = parsedData.email;
+        refs.textarea.value = parsedData.textarea;
+    };
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(storageValues);
+};
